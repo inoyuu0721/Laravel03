d6fc06f58170f54f604d8b83a0a934eea5d46605
“inoueyuichi” <inoyuu0721@gmail.com>
2021-02-19 03:16:01 +0000

    修正追加

--- index.php
+++ index.php
@@ -14,12 +14,14 @@ if ($name === 'inoue'){
  echo "\n";
 }
 
-// 問題2
+// 問題2 for文を使って、1から10000までの合計の値を表示してください
 $total = 0;
-for ($i = 0; $i <= 10000; $i++) {
-echo  $total += $i;
-echo "\n";
+
+for ($i = 1; $i <= 10000; $i++) {
+ $total += $i;
 }
+echo $total;
+echo "\n";
 
 // 問題3
 $fruits = array('apple','banana','lemon','grape','melon');
